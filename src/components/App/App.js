add999b5b0c53cfdb415c6499c5a6a6d53274fac
Stefano Sotelo <stefano.sotelo@pucp.pe>
2021-02-11 15:14:27 +0100

    Initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,35 +1,27 @@
 import React, { Component } from "react";
-import { Layout } from "antd";
 import TopBar from "../TopBar/TopBar";
 import SystemDate from '../SystemDate/SystemDate';
-import "antd/dist/antd.css";
 import styles from "./App.module.css";
 
 class App extends Component {
 
   render() {
-
-    // Layout elements
-    const { Header, Footer, Sider, Content } = Layout;
-
-    // Top bar elements (I pass them as props)
-    const topBarProps = {
-      logo: 'logo value',
-      date: <SystemDate className={styles.h50} />,
-      closeButton: 'close button'
-    };
-
     return (
-      <Layout className={styles.app}>
-        <Header className={styles.header}>
-          <TopBar {...topBarProps} />
-        </Header>
-        <Layout>
-          <Sider theme="light" className={styles.sideBar}>SideBar</Sider>
-          <Content className={styles.screen}>Screen</Content>
-        </Layout>
-        <Footer className={styles.footer} />
-      </Layout>
+      <div className={styles.app}>
+        <header className={styles.header}>
+          <TopBar 
+            logo='logo'
+            close='close'
+          
+
+          />
+        </header>
+        <main className={styles.main}>
+          <aside className={styles.sideBar}>SideBar</aside>
+          <section className={styles.screen}>Screen</section>
+        </main>
+        <footer className={styles.footer} />
+      </div>
     );
   }
 }
